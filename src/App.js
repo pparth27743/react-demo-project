2b769a5422ff2e6a857a5c5d7b7eb5376b04ea4f
Parth Patel <62556601+pparth27743@users.noreply.github.com>
2022-03-21 22:59:47 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,115 +1,24 @@
-import Header from "./components/Header";
-import Tasks from "./components/Tasks";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { useState, useEffect } from "react";
-import AddTask from "./components/AddTask"
-import Footer from "./components/Footer";
-import About from "./components/About";
-import { ClipLoader } from "react-spinners";
-
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [showAddTask, setShowAddTask] = useState(false)
-  const [tasks, setTasks] = useState([]);
-  const [loading, setLoading] = useState(false)
-
-  const url = 'http://localhost:5000/tasks'
-
-  useEffect(() => {
-    setLoading(true)
-    setTimeout(() => {
-      const getTasks = async () => {
-        const tasks = await fetchTasks()
-        setTasks(tasks)
-        setLoading(false)
-      }
-      getTasks()
-
-    }, 1000);
-
-  }, [])
-
-  // Fetch all tasks
-  const fetchTasks = async () => {
-    const res = await fetch(`${url}`)
-    const data = await res.json()
-    return data
-  }
-
-  // Fetch a task
-  const fetchTask = async (id) => {
-    const res = await fetch(`${url}/${id}`)
-    const data = await res.json()
-    return data
-  }
-
-  //Delete Task
-  const deleteTask = async (id) => {
-    await fetch(`${url}/${id}`, { method: 'DELETE' })
-
-    setTasks(tasks.filter((task) => task.id !== id));
-  };
-
-  // Toggle Reminder
-  const toggleReminder = async (id) => {
-
-    const data = await fetchTask(id)
-    const updatedData = { ...data, reminder: !data.reminder }
-
-    await fetch(`${url}/${id}`, {
-      method: 'PUT',
-      headers: {
-        'Content-type': 'application/json'
-      },
-      body: JSON.stringify(updatedData)
-    })
-
-    setTasks(
-      tasks.map((task) =>
-        task.id === id ? { ...task, reminder: updatedData.reminder } : task
-      )
-    );
-  };
-
-  // Add Task
-  const addTask = async (task) => {
-
-    const res = await fetch(`${url}`, {
-      method: 'POST',
-      headers: {
-        'Content-type': 'application/json'
-      },
-      body: JSON.stringify(task)
-    })
-    const data = await res.json()
-
-    setTasks([...tasks, data])
-
-    // const id = Math.floor(Math.random() * 10000) + 1
-    // const newTask = { id, ...task }
-    // setTasks([...tasks, newTask])
-  };
-
   return (
-    <Router>
-      {loading && <div className="loader" >
-        <ClipLoader color={'#4A83E2'} loading={loading} size={150} />
-      </div>}
-
-      {!loading && <div className="container">
-        <Header showAdd={showAddTask} onAdd={() => setShowAddTask(!showAddTask)} />
-        {showAddTask && <AddTask onAdd={addTask} />}
-        {tasks.length > 0 ? (
-          <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
-        ) : (
-          "No tasks to show"
-        )}
-        {/* <Routes>
-          <Route path='/about' element={<About />} />
-        </Routes> */}
-        <Footer />
-      </div>}
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,15 +3,124 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+  Link,
+  Outlet,
+  useParams,
+  NavLink,
+  useNavigate,
+  useLocation
+} from 'react-router-dom'
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/myapps' element={<Navigate replace to='/learn' />} />
+        <Route path='/learn' element={<Learn />} >
+          <Route path='course' element={<Course />} >
+            <Route path=':courseid' element={<CourseId />} />
+          </Route>
+          <Route path='bundle' element={<Bundle />} />
+        </Route>
+        <Route path='/dashboard' element={<Dashboard />} />
+      </Routes>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+
+
+
+function Home() {
+  return (
+    <div>
+      <h1> Home Route</h1>
+    </div>
+  )
+}
+
+function Learn() {
+  return (
+    <div>
+      <h1> Learn Route</h1>
+      <h4>All course are lsited here</h4>
+      <Link className='btn btn-success' to="/learn/course"> Courses </Link> |
+      <Link className='btn btn-primary' to="/learn/bundle"> Bundle </Link>
+      <Outlet />
+    </div>
+  )
+}
+
+
+function Bundle() {
+  return (
+    <div>
+      <h1> Bundle Route</h1>
+    </div>
+  )
+}
+
+function Course() {
+
+  const courseList = ['React', 'Angular', 'Vue', 'Nodejs']
+  const randomCourseName = courseList[Math.floor(Math.random() * courseList.length)]
+
+  return (
+    <div>
+      <h1> Course Route</h1>
+
+      <NavLink
+        style={({ isActive }) => ({
+          backgroundColor: isActive ? 'green' : 'yellow',
+        })}
+        to={`/learn/course/${randomCourseName}`}
+      >
+        {randomCourseName}
+      </NavLink>
+
+      <Outlet />
+    </div >
+  )
+}
+
+function CourseId() {
+  const navigate = useNavigate()
+  const { courseid } = useParams()
+  return (
+    <div>
+      <h1> URL Param is : {courseid}</h1>
+      <button 
+      onClick={ () => {
+        navigate('/dashboard', {state : {name:'parth'}})
+      }}
+      className='btn btn-warning'>Price</button>
+
+    <Link to='/dashboard' state={'Nodejs'} className='btn btn-primary'> Price</Link>      
+    </div>
+  )
+}
+
+function Dashboard() {
+  const location = useLocation()
+  console.log(location)
+  return (
+    <div>
+      <h1> Dashboard {location.state.name}</h1>
+    </div>
+  )
+}
+
+
+
 reportWebVitals();
+
+
